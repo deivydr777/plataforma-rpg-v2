dade8ec313a2aa75ba497d5e25dbe81868b74357
deivydr777 <deivydrodrigues718@gmail.com>
2025-08-01 00:53:46 -0300

    update frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './Home'; // Importamos a Home diretamente
+import { BrowserRouter } from 'react-router-dom'; // O useNavigate dentro da Home precisa disso
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    {/* Envolvemos a Home com o BrowserRouter para o 'navigate' não quebrar */}
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
